7263e08bc439d49ec875b052677d568c546031f3
Patrick Souza <patrick_ams@hotmail.com>
2023-08-02 21:32:04 -0300

    added loading function

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import streamlit as st 
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import pipeline
+import torch 
+import base64 
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.vectorstores import Chroma 
+from langchain.llms import HuggingFacePipeline
+from langchain.chain import RetrievalQA
+
 
 checkpoint = "MBZUAI/LaMini-Flan-T5-77M"
 
